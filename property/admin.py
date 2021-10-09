3ef9504d9ea7efc5498ef293b295e91529f3d439
stanislavglazko <stanislav.glazko@dualbootpartners.com>
2021-10-09 10:42:59 +0300

    Customize Flat Admin

---
+++ property/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from .models import Flat
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    search_fields = ('owner', 'town', 'address', )
+    readonly_fields = ['created_at']
+
+
+admin.site.register(Flat, AuthorAdmin)
